c43d33361b43933e09390bca59a9441c14ebc12c
nissene99 <enissenacc@protonmail.com>
2022-11-25 18:15:40 +0100

    update

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from dataclasses import Field
+import math
 
 import pygame
 
 
 class Table:
-    def __init__(self, width, height, cell_size):
+    def __init__(self, width, height, cell_size, screen):
         self.width = width
         self.height = height
         self.cell_size = cell_size
@@ -13,15 +13,17 @@ class Table:
         self.end_pos = self.random_end_pos()
         self.actual_pos = self.start_pos
         self.visited = []
-        
+        self.screen = screen
+        self.steps = 0
+
     def get_cell(self, pos):
         x, y = pos
         return self.cells[y][x]
-    
+
     def set_cell(self, pos, value):
         x, y = pos
         self.cells[y][x] = value
-        
+
     def get_neighbours(self, pos):
         x, y = pos
         neighbours = []
@@ -32,110 +34,164 @@ class Table:
                 if 0 <= x + i < self.width and 0 <= y + j < self.height:
                     neighbours.append(self.cells[y + j][x + i])
         return neighbours
-    
+
     def get_click(self, mouse_pos):
         x, y = mouse_pos
         return x // self.cell_size, y // self.cell_size
-    
+
     def render(self, screen):
         for y in range(self.height):
             for x in range(self.width):
                 if self.cells[y][x] == 0:
-                    pygame.draw.rect(screen, pygame.Color('black'), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size), 1)
+                    pygame.draw.rect(
+                        screen, pygame.Color('black'),
+                        (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size), 1
+                        )
                 else:
-                    pygame.draw.rect(screen, pygame.Color('green'), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
-                    
+                    pygame.draw.rect(
+                        screen, pygame.Color('green'),
+                        (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size)
+                        )
 
-     # returns a x, y coordinate of a random cell where the bot starts
+    # returns a x, y coordinate of a random cell where the bot starts
     def random_start_pos(self):
         import random
         x = random.randint(0, self.width - 1)
         y = random.randint(0, self.height - 1)
         return x, y
-                
+
     def random_end_pos(self):
         import random
         x = random.randint(0, self.width - 1)
         y = random.randint(0, self.height - 1)
         return x, y
-    
+
     def draw_path(self, screen, path):
         for pos in path:
-            pygame.draw.rect(screen, pygame.Color('red'), (pos[0] * self.cell_size, pos[1] * self.cell_size, self.cell_size, self.cell_size))
-            
+            pygame.draw.rect(
+                screen, pygame.Color('red'),
+                (pos[0] * self.cell_size, pos[1] * self.cell_size, self.cell_size, self.cell_size)
+                )
+
     def go_left(self):
         x, y = self.actual_pos
         if x > 0:
             self.actual_pos = x - 1, y
             self.visited.append(self.actual_pos)
-    
+
     def go_right(self):
         x, y = self.actual_pos
         if x < self.width - 1:
             self.actual_pos = x + 1, y
             self.visited.append(self.actual_pos)
-            
+
     def go_up(self):
         x, y = self.actual_pos
         if y > 0:
             self.actual_pos = x, y - 1
             self.visited.append(self.actual_pos)
-            
+
     def go_down(self):
         x, y = self.actual_pos
         if y < self.height - 1:
             self.actual_pos = x, y + 1
             self.visited.append(self.actual_pos)
-            
+
     def draw_actual_pos(self, screen):
         x, y = self.actual_pos
-        pygame.draw.rect(screen, pygame.Color('blue'), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
-        
+        pygame.draw.rect(
+            screen, pygame.Color('blue'), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size)
+            )
+
     def draw_start_pos(self, screen):
         x, y = self.start_pos
-        pygame.draw.rect(screen, pygame.Color('yellow'), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
-        
+        pygame.draw.rect(
+            screen, pygame.Color('yellow'), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size)
+            )
+
     def draw_end_pos(self, screen):
         x, y = self.end_pos
-        pygame.draw.rect(screen, pygame.Color('green'), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
-        
-    def draw_visited(self, screen):
+        pygame.draw.rect(
+            screen, pygame.Color('green'), (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size)
+            )
+
+    def draw_visited(self):
         for pos in self.visited:
-            pygame.draw.rect(screen, pygame.Color('blue'), (pos[0] * self.cell_size, pos[1] * self.cell_size, self.cell_size, self.cell_size))
-        
+            self.draw_text_in_cell(math.pow(0.91, self.steps), pos)
+            x, y = pos
+            
+            r = pygame.Surface((self.cell_size, self.cell_size))
+            r.set_alpha(1)
+            r.fill(pygame.Color('red'))
+            self.screen.blit(r, (x * self.cell_size, y * self.cell_size))
+
+    def draw_text_in_cell(self, text, pos):
+        # draw the text in the cells in the middle
+        pygame.font.init()
+        font = pygame.font.SysFont('Arial', 20)
+        text = font.render(f"{text}.4f", True, pygame.Color('black'))
+        text_rect = text.get_rect(center=(pos[0] * self.cell_size + self.cell_size // 2, pos[1] * self.cell_size + self.cell_size // 2))
+        self.screen.blit(text, text_rect)
+
+    def is_visited(self, pos):
+        #return False
+        return pos in self.visited
+
     def random_step(self):
         import random
         x, y = self.actual_pos
         rand = random.randint(0, 3)
         if rand == 0:
             self.go_left()
+            self.steps += 1
         elif rand == 1:
             self.go_right()
+            self.steps += 1
         elif rand == 2:
             self.go_up()
-        else:
+            self.steps += 1
+        elif rand == 3:
             self.go_down()
-    
-    def train(self, screen):
-        self.draw_start_pos(screen)
-        self.draw_end_pos(screen)
+            self.steps += 1
+        self.set_cell(self.actual_pos, 1)
+        
+        
+    def draw_steps(self):
+        # draws the number of steps in the bottom left corner
+        pygame.font.init()
+        font = pygame.font.SysFont('Arial', 20)
+        text = font.render(f"Step: {self.steps}", True, pygame.Color('black'))
+        text_rect = text.get_rect(center=(self.width * self.cell_size // 2, self.height * self.cell_size + self.cell_size // 2))
+        self.screen.blit(text, text_rect)
+
+
+    def update(self):
+        #self.clear()
+        self.set_cell(self.start_pos, 1)
+        self.set_cell(self.end_pos, 1)
+        # draw the text in the cells
+        self.draw_text_in_cell('S', self.start_pos)
+        self.draw_text_in_cell('E', self.end_pos)
+        self.draw_start_pos(self.screen)
+        self.draw_end_pos(self.screen)
+        self.draw_visited()
+        self.draw_steps()
+        pygame.display.flip()
+
+    def train(self):
         while self.actual_pos != self.end_pos:
             self.random_step()
-            self.draw_visited(screen)
-        return self.visited
-            
-        
-                
+            self.update()
+
     def clear(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                self.cells[y][x] = 0
+        self.screen.fill(pygame.Color('white'))
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
-    window = pygame.display.set_mode((640, 480))
+    window = pygame.display.set_mode((1000, 1000))
     # create a table of 16x16 cells
-    table = Table(16, 16, 32)
+    table = Table(16, 16, 60, window)
     running = True
     while running:
         for event in pygame.event.get():
@@ -146,7 +202,4 @@ if __name__ == '__main__':
                 table.set_cell(cell, 1)
         window.fill(pygame.Color('white'))
         table.render(window)
-        table.train(window)
-        pygame.display.flip()
-    
-    
+        table.train()
