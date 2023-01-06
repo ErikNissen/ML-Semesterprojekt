8841e5f57a0df1a11f5f1689489d27d7d4223abc
nissene99 <enissenacc@protonmail.com>
2023-01-06 18:02:54 +0100

    update

--- src/Graphical.py
+++ src/Graphical.py
@@ -1,34 +1,36 @@
-# File: Graphical.py
-
 import asyncio
 import sys
 import threading
 from random import randint
 from time import sleep
 
-from PyQt5 import Qt
-from PyQt5.QtGui import QColor, QFont, QTextOption
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor, QFont, QTextCharFormat, QTextOption
 from PyQt5.QtWidgets import (QAbstractItemView, QApplication, QHBoxLayout,
                              QHeaderView,
                              QLineEdit, QMainWindow, QPushButton,
                              QRadioButton,
-                             QSizePolicy, QTableWidget,
+                             QTableWidget,
                              QTableWidgetItem,
-                             QTextEdit, QVBoxLayout, QWidget)
+                             QTextEdit, QToolBar, QVBoxLayout, QWidget)
 
 from src.Log import LogWindow
 from src.modes import Mode
 from src.Robot import Robot
-class MainWindow(QWidget):
-    def __init__(self, title, width, height, left=0, top=0, row=16, col=16,
-                 hz=60):
+from colorama import Fore, Style
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, title, width, height, left=0, top=0):
         super().__init__()
-        self.log = None
-        self.refreshrate = hz
+        self.logViewer = QTextEdit()
+        self.btns = []
+        self.robotPosition = None
         self.hindernisswidth = None
         self.hindernissheight = None
         self.hindernissY = None
         self.hindernissX = None
+        self.robot = None
         self.iterations = 1
         self.tbl = QTableWidget()
         self.title = title
@@ -36,50 +38,116 @@ class MainWindow(QWidget):
         self.height = height
         self.left = left
         self.top = top
-        self.tableColumn = col  # create Feld default
-        self.tableRow = row  # create Feld default
+        self.tableColumn = 16  # create Feld
+        self.tableRow = 16  # create Feld
         self.endPoint = (0, 0)
         self.startPoint = (0, 0)
-        self.btns = []
         self.mode = Mode.EXPLORE
         self.boolFindWay = False
 
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.table(row=self.tableRow, column=self.tableColumn)  # create Feld
 
         self.randomStartPoint()  # create startPoint
         self.randomEndPoint()  # create endPoint
 
+        self.initRobot()
+
+        # Create a tool bar and add the buttons as actions
+        self.toolbar = QToolBar(self)
+        self.logtoolbar = QToolBar(self)
+
+        self.initToolbar()
+
+        # Add the table widget to the central widget
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tbl)
+
+        centralWidget = QWidget()
+        centralWidget.setLayout(self.layout)
+        self.setCentralWidget(centralWidget)
+        self.resize(self.tbl.width() +
+                    self.toolbar.width() +
+                    self.logtoolbar.width() +
+                    800,
+                    max(self.tbl.height(), self.toolbar.height(),
+                        self.logtoolbar.height()))
+        self.show()
+
+    def initRobot(self):
         self.robot = Robot(self.startPoint, self.endPoint,
                            self.tbl)  # create Robot
         self.robotPosition = self.robot.getPos()
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.tbl)
-        self.buttons()
-        self.hindernissMenu()
-        for btn in self.btns:
-            self.layout.addWidget(btn)
-        self.setLayout(self.layout)
-        # make background of tbl black
-        self.setStyleSheet("background-color: rgb(75,75,75);")
+    def initToolbar(self):
+        self.addToolBar(Qt.LeftToolBarArea, self.toolbar)
 
-        # make grid of tbl white
-        self.tbl.setStyleSheet(
-                "QTableView {gridline-color: rgb(200,200,200);}")
-        self.show()
+        # Define constants for button positions
+        BTN_X_POS = 0
 
-    def LogViewer(self):
-        return
-        if self.log is None:
-            self.log = LogWindow()
-        self.log.show()
+        # Define a list of tuples for the buttons
+        buttons = [
+                ("Start", self.restart, (BTN_X_POS, 0)),
+                ("Automatic", self.automatic, (BTN_X_POS, 100)),
+                ("Random Start/End Punkt", self.rdmStartEnd, (BTN_X_POS,
+                                                              200)),
+                ("Exit", self.close, (BTN_X_POS, 200))]
+
+        # Add buttons to toolbar
+        for label, callback, pos in buttons:
+            self.toolbar.addWidget(self.createButton(label, callback, pos))
+
+        # Create a widget to hold the radio buttons and input field
+        widget = QWidget()
+
+        # Use a horizontal layout to position the radio buttons and input field
+        layout = QHBoxLayout()
+        layout.addWidget(
+                self.createInputField("Iteration", (0, 300),
+                                      self.setIterations))
+        layout.addWidget(self.createRadioButton("Explorer",
+                                                self.explorerMode, (0, 0)))
+        layout.addWidget(self.createRadioButton("Find Way",
+                                                self.findWayMode, (0, 1)))
+
+        widget.setLayout(layout)
+
+        # Add the widget to the tool bar
+        self.toolbar.addWidget(widget)
+
+        # Set the orientation of the toolbar to be vertical
+        self.toolbar.setOrientation(Qt.Vertical)
+
+        self.initLog()
+        self.addToolBar(Qt.RightToolBarArea, self.logtoolbar)
+
+    def initLog(self):
+        self.logViewer.setFixedHeight(self.size().height())
+        self.logtoolbar.addWidget(self.logViewer)
+
+    def log(self, info, color=Qt.white, bold=False):
+        charFormat = QTextCharFormat()
+        charFormat.setFontPointSize(10)
+        charFormat.setForeground(color)
+        if bold:
+            charFormat.setFontWeight(QFont.Bold)
+        cur = self.logViewer.textCursor()
+        cur.insertText(info, charFormat)
+        cur.movePosition(cur.End)
+        self.logViewer.ensureCursorVisible()
+        self.logViewer.adjustSize()
+        self.logViewer.update()
+        self.logtoolbar.adjustSize()
+        self.logtoolbar.update()
+        self.resize(self.tbl.columnCount()*50 +
+                    self.toolbar.width() +
+                    self.logtoolbar.width(),
+                    max(self.tbl.height(), self.toolbar.height(),
+                        self.logtoolbar.height()))
+        self.update()
 
     def buttons(self):
-        self.btns.append(
-                self.createButton("Show Log", self.LogViewer, (0, 200)))
         self.btns.append(self.createButton("Start", self.restart, (0, 0)))
         self.btns.append(self.createButton("Automatic", self.automatic, (0,
                                                                          100)))
@@ -87,17 +155,6 @@ class MainWindow(QWidget):
                 self.createButton("Random Start/End Punkt", self.rdmStartEnd,
                                   (0, 200)))
         self.btns.append(self.createButton("Exit", self.close, (0, 200)))
-
-        resizetbllayout = QHBoxLayout()
-        rowinput = self.createInputField("Row", (0, 200),
-                                         self.setrow)
-        colinput = self.createInputField("Col", (0, 200),
-                                         self.setcol)
-        resizetbllayout.addWidget(rowinput)
-        resizetbllayout.addWidget(colinput)
-        resizetbllayout.addWidget(self.createButton("Resize Table",
-                                                    self.resizetbl, (0, 200)))
-        self.layout.addLayout(resizetbllayout)
 
         radiobtnlayout = QHBoxLayout()
 
@@ -109,35 +166,9 @@ class MainWindow(QWidget):
         radiobtnlayout.addWidget(iteration)
         radiobtnlayout.addWidget(explore)
         radiobtnlayout.addWidget(self.createRadioButton(
-                "Find Way", self.findWayMode, (0, 400)))
+                "Find Way", self.findWayMode, (0, 400)))  # "Find Way" kann
+        # nur benutzt werden, wenn mindestens 1 Explor gemacht wurde
         self.layout.addLayout(radiobtnlayout)
-
-    #   ToDo 7: Hindernisse einbauen
-    #     (Optional) Sowohl random als auch feste strecken
-
-    def resizetbl(self):
-        self.clearTable()
-        print("Resize Table")
-        self.table(self.tableRow, self.tableColumn)
-        self.startPoint = None
-        self.endPoint = None
-        print("Create new Startpoint")
-        self.randomStartPoint()  # create startPoint
-        print("Create new Endpoint")
-        self.randomEndPoint()  # create endPoint
-        print("Create new Robot")
-        self.robot = Robot(self.startPoint, self.endPoint,
-                           self.tbl)  # create Robot
-        print("Get Robot Pos")
-        self.robotPosition = self.robot.getPos()
-
-    def setrow(self, row):
-        if row.isdigit() and int(row) > 0:
-            self.tableRow = int(row)
-
-    def setcol(self, col):
-        if col.isdigit() and int(col) > 0:
-            self.tableColumn = int(col)
 
     def hindernissMenu(self):
         layout = QHBoxLayout()
@@ -188,10 +219,6 @@ class MainWindow(QWidget):
             radius = self.tableRow
         self.hindernissheight = int(height)
 
-    #   ToDo 7: Hindernisse einbauen
-    #     WICHTIG!! Hindernisse müssen so gebaut sein das man noch zum Ziel
-    #     kommt bei user eingabe Fehlermeldung den User geben, bei random
-    #     erneuter versuch ein Hinderniss einzubauen
     def checkHinterniss(self):
         # Check if the start or end point is in hinderniss
         if self.hindernissX is not None and self.hindernissY is not None and self.hindernisswidth is not None and self.hindernissheight is not None:
@@ -226,19 +253,10 @@ class MainWindow(QWidget):
             print("Start or End point is in hinderniss")
 
     def automatic(self):
-        counter = self.iterations
-        counter_explore = 0
-        counter_findWay = 0
-
-        while counter_explore < counter:
-            self.mode = Mode.EXPLORE
-            self.restart(iterations=1, delay=1/self.refreshrate)
-            counter_explore += 1
-
-        while counter_findWay < counter:
-            self.mode = Mode.BYPOINTS
-            self.restart(iterations=1, delay=1/self.refreshrate)
-            counter_findWay += 1
+        self.mode = Mode.EXPLORE
+        self.restart(0)
+        self.mode = Mode.BYPOINTS
+        self.restart(0)
 
     def explorerMode(self):
         print("Explorer Mode")
@@ -271,7 +289,7 @@ class MainWindow(QWidget):
         success = False
         while not success:
             try:
-                self.startPoint = (column, row)
+                self.startPoint = (row, column)
 
                 self.tbl.setItem(row, column, QTableWidgetItem("Start"))
                 self.tbl.item(row, column).setBackground(QColor(0, 255, 0))
@@ -289,7 +307,7 @@ class MainWindow(QWidget):
         # check if End Point is not Start Point
         while not success:
             try:
-                self.endPoint = (column, row)
+                self.endPoint = (row, column)
                 self.tbl.setItem(row, column, QTableWidgetItem("End"))
                 self.tbl.item(row, column).setBackground(QColor(255, 0, 0))
                 success = True
@@ -318,87 +336,54 @@ class MainWindow(QWidget):
         self.tbl.resizeRowsToContents()
 
     def run(self, delay=1 / 144):
-        cancel = False
-        try:
-            while self.robotPosition != self.endPoint:
-                if self.mode == Mode.EXPLORE:
-                    self.robot.randomMove()
-                    self.boolFindWay = True
-                elif self.mode == Mode.BYPOINTS:
-                    if self.boolFindWay:
-                        try:
-                            self.robot.movebyPoints()
-                        except:
-                            cancel = True
-                            break
-                    else:
-                        print("Need to Explorer First!")
-                        break
-                self.robotPosition = self.robot.getPos()
-                if self.robotPosition not in self.robot.visited:
-                    self.robot.visited.append(self.robotPosition)
-                self.robot.steps += 1
-                # print(f"Robot Position: {self.robotPosition}")
-                # print(f"Old Robot Position: {self.robot.getoldPos()}")
-                self.update()
-                sleep(delay)
-            print(f"Robot Steps: {self.robot.steps}")
-            if cancel:
-                print(f"Robot stops at position {self.robot.getPos()}")
+        while self.robotPosition != self.endPoint:
+            if self.mode == Mode.EXPLORE:
+                self.robot.randomMove()
+                self.boolFindWay = True
+            elif self.mode == Mode.BYPOINTS:
+                if self.boolFindWay:
+                    self.robot.movebyPoints()
+                else:
+                    print("Need to Explorer First!")
+                    break
+            self.robotPosition = self.robot.getPos()
+            if self.robotPosition not in self.robot.visited:
+                self.robot.visited.append(self.robotPosition)
+            self.robot.steps += 1
+            self.log("Robot Position:", Qt.cyan, bold=True)
+            self.log(f" {self.robotPosition}\n")
+            self.update()
+            sleep(delay)
+        self.log("Robot Steps:", Qt.green, bold=True)
+        self.log(f"{self.robot.steps}\n")
 
-            # ToDO: WIP ToDo 2: Punkte vergabe: wenn im Feld bereits punkte vorhanden
-            #  sind nehme den Mittelwert aus beiden Zahlen
-            backvisited = self.robot.visited[::-1]
-            for b in range(len(backvisited)):
-                back = self.robot.visited[b]
-                if back == self.endPoint or back == self.startPoint:
-                    continue
-                points = 0.9 ** b
+        backvisited = self.robot.visited[::-1]
+        for b in range(len(backvisited)):
+            if backvisited[b] == self.endPoint:
+                continue
 
-                cellPoints = None
-                try:
-                    if self.mode == Mode.BYPOINTS:
-                        print(f"value of back is: {back}") # Debugging
-                        item = self.tbl.item(back[1], back[0])
-                        print(f"item: {item}")  # Debugging
-                        cP = item.text()
-                        print(f"text cP: {cP}")  # Debugging
-                        cellPoints = float(cP)
-                        print(f"cellPoints: {cellPoints}")  # Debugging
-                except:
-                    cellPoints = None
+            points = 0.9 ** b
+            cellPoints = 0
+            try:
+                # get CellPoints value
+                cellPoints = float(
+                        self.tbl.item(backvisited[b][0],
+                                      backvisited[b][1]).text())
+            except:
+                pass
+            # if cellPoints != 0
+            if cellPoints != 0:
+                # median = (points+"cellpoints")/2
+                median = (points + cellPoints) / 2
+                # points += median
+                points += median
 
-                # Berechnen des neuen Punktwerts für die Zelle
-                if cellPoints is not None:
-                    # median = (points + cellPoints)/2
-                    median = (points + cellPoints) / 2
-                    print(f"{median} = ({points} + {cellPoints}) / 2") # Debugging
-                    # points += median
-                    points += median
-                    print(f"{points} += {median}") # Debugging
-                elif cellPoints == 0:
-                    points = 0
-
-                if not cancel:
-                    print(f"{b}: {points} bei Position:{self.robot.visited[b]}")
-
-                # Setzen des Punktwerts in der Tabelle
-                self.setPoints(points, back)
-            for row in range(self.tableRow):
-                for column in range(self.tableColumn):
-                    cell = row, column
-                    if cell == self.startPoint or cell == self.endPoint:
-                        continue
-                    else:
-                        pass
-                    try:
-                        item = self.tbl.item(row, column)
-                        float(item.text())
-                    except:
-                        self.tbl.setItem(row, column, QTableWidgetItem("-1"))
-                self.update(end=True)
-        except Exception as e:
-            print(e)
+            self.log(f"{b}:", Qt.yellow)
+            self.log(f" {points:.2e} ", Qt.magenta, bold=True)
+            self.log("Punkte bei Position:"
+                     f" {self.robot.visited[b]}\n")
+            self.setPoints(points, backvisited[b])
+            self.update(end=True)
 
     def clearTable(self):
         for row in range(self.tbl.rowCount()):
@@ -413,11 +398,8 @@ class MainWindow(QWidget):
                 except:
                     pass
 
-    def restart(self, delay=1 / 60, iterations=None):
-        delay = 1/self.refreshrate
-        self.LogViewer()
-        for _ in range(0,
-                       self.iterations if iterations is None else iterations):
+    def restart(self, delay=1 / 144):
+        for _ in range(0, self.iterations):
             for x in range(self.tableRow):
                 for y in range(self.tableColumn):
                     try:
@@ -466,13 +448,6 @@ class MainWindow(QWidget):
                     oldcolor = None
                 self.paintCell(i[0], i[1], QColor(0, 0, 255, 1), "", oldcolor,
                                1)
-            self.robotPosition = self.robot.getPos()
-            try:
-                self.tbl.item(self.robotPosition[0], self.robotPosition[
-                    1]).background()
-            except AttributeError:
-                self.tbl.setItem(self.robotPosition[0], self.robotPosition[
-                    1], QTableWidgetItem(""))
             alpha = self.tbl.item(self.robotPosition[0], self.robotPosition[
                 1]).background().color().alpha()
             self.paintCell(self.robotPosition[0], self.robotPosition[1],
@@ -481,12 +456,3 @@ class MainWindow(QWidget):
                            QColor(0, 255, 0), "S", alpha=255)
             self.paintCell(self.endPoint[0], self.endPoint[1],
                            QColor(255, 0, 0), "E", alpha=255)
-
-    def showEvent(self, event) -> None:
-        size = self.sizeHint()
-        tblSize = self.tbl.sizeHint()
-        width = size.width() + tblSize.width()
-        height = size.height() + tblSize.height() + 50
-        size.setWidth(width)
-        size.setHeight(height)
-        self.resize(size)
